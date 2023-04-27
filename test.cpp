8474d3ed394764552705954d99fa680e82083738
Anhelina Kulkova <a.kulkova.ip21@kpi.ua>
2023-04-27 19:19:17 +0300

    Directory reader
    dir_reader takes a path to folder (here on the desktop, but content od the folder is in files 1.csv and 2.csv) and returns a vector of strings with the names of files in the directory. There is also possibility for recursive reading of a directory. So the read_record function now reads from all files in the directory and create_rating works in an append mode

--- test.cpp
+++ test.cpp
@@ -3,27 +3,46 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <dirent.h>
+
 
 using namespace std; 
 
-
-void read_record(); 
+void read_record(vector<string> files,  const char* initial_path); 
 void create_rating(int, float*, vector<string>); 
+vector<string> dir_reader(const char*);
 
 int main(){
 
+    const char* dirname = "/Users/angelina/Desktop/students/";  
+    vector<string> filenames;
+    filenames =dir_reader(dirname);
 
-    read_record(); 
+    read_record(filenames, dirname); 
+    
     // create(); - inside read_record
         // will accept the list of avarage marks and names/numbers in old file of students
 }
 
-void read_record()
-{
+//update try catch with proper cerr
+void read_record(vector<string> files, const char* initial_path)
+{   
+    for(int i = 0; i < files.size(); i++){
+    string filename = initial_path + files[i]; 
+    cout << filename << endl;
     // File pointer
     fstream fin;
     // Open an existing file
-    fin.open("students1.csv", ios::in);
+   
+    try
+    {
+        fin.open(filename, ios::in);
+    }
+    catch(const std::exception& e)
+    {
+        // std::cerr << e.what() << "Error when opening the file"<<'\n';
+        cout <<  "Error when opening the file" << endl;
+    }
 
     //if on budget -> calculate avarage
   
@@ -35,7 +54,8 @@ void read_record()
     string c;
     getline(fin, c); 
     cout << c << endl; 
-    int students_number = stoi(c); 
+    // int students_number = stoi(c); 
+    int students_number = 40;
     int bdgt_student_count = 0;
 
     float* average_list = new float[students_number];
@@ -43,7 +63,7 @@ void read_record()
 
     // Read the Data from the file
     // as String Vector
-    vector<string> record;
+    vector<string> record; // could be char** array
     vector<string> names;
     string line, word;
   
@@ -95,26 +115,66 @@ void read_record()
     fin.close(); 
 
     create_rating(bdgt_student_count, average_list, names); 
+    
+    }
 }
 
 void create_rating(int st_count, float* grade, vector<string> st_names){
     fstream fout;
+
     try
     {
-        fout.open("rating.csv", ios::out);
+        fout.open("rating1.csv", ios::app);
+        //fout.open("rating.csv", ios::app); // here the probl with the first record
+            // with st_count 
     }
     catch(const std::exception& e)
     {
         // std::cerr << e.what() << "Error when opening the file"<<'\n';
         cout <<  "Error when opening the file"<< endl;
     }
-    fout << st_count << "\n";
+    
+    // fout << st_count << "\n";
 
     for(int i = 0; i < st_count; i++){
         fout << st_names[i] << "," << grade[i]<<"\n";
     }
-
-
     
     fout.close(); 
+}
+
+// functoin to sort students acc to their grade, 
+// count 40% of budget students and extracting the minimal mark 
+vector<string> dir_reader(const char* dirname){
+    vector<string> filenames;
+    DIR* dir = opendir(dirname); 
+    if(dir == NULL){
+        cout<< "An empty directory "<< dirname <<  endl; 
+    }
+    else{
+        struct dirent* inside_dir; 
+        inside_dir = readdir(dir); 
+
+        while(inside_dir != NULL) {
+           
+            if(inside_dir->d_type == DT_REG){
+                filenames.push_back(inside_dir->d_name);
+                cout << inside_dir->d_name << endl; 
+            }
+            if(inside_dir->d_type == DT_DIR && strcmp(inside_dir->d_name, ".")!= 0  && strcmp(inside_dir->d_name, "..")!= 0){
+                char recursive_dir[100] = {0};
+                strcat(recursive_dir, dirname); 
+                strcat(recursive_dir, "/"); 
+                strcat(recursive_dir, inside_dir->d_name);
+
+                dir_reader(recursive_dir); 
+            }
+           
+            inside_dir = readdir(dir);
+            
+        }
+        
+    }
+    closedir(dir);
+    return filenames; 
 }
